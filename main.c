070a06977e704dd1dfc1648d61c8b437a0962d2e
dkfv <1418563174@qq.com>
2022-06-22 11:02:21 +0800

    test

--- main.c
+++ main.c
@@ -1,17 +1,207 @@
-#include <regx52.h>
-#include "dk.h"
+#include <REGX52.H>
 #include "LCD1602.h"
-#include "At24C02.h"
-#include "I2C.h"
 #include "ds1302.h"
-#include "timeclock.h"
-typedef unsigned int ui;
-typedef unsigned char uc;
-// uc Hour = 23, Min = 59, Sec = 58;
-// uc Year = 2001, Mon = 3, Day = 30;
-uc KeyNum, MODE, TimeSetSelect, TimeSetFlashFlag;
-// void showtime();
-// void read();
+#include "dk.h"
+
+unsigned char KeyNum, MODE, TimeSetSelect, TimeSetFlashFlag;
+
+void TimeShow(void) //时间显示功能
+{
+	DS1302_ReadTime();					  //读取时间
+	LCD_ShowNum(1, 1, DS1302_Time[0], 2); //显示年
+	LCD_ShowNum(1, 4, DS1302_Time[1], 2); //显示月
+	LCD_ShowNum(1, 7, DS1302_Time[2], 2); //显示日
+	LCD_ShowNum(2, 1, DS1302_Time[3], 2); //显示时
+	LCD_ShowNum(2, 4, DS1302_Time[4], 2); //显示分
+	LCD_ShowNum(2, 7, DS1302_Time[5], 2); //显示秒
+}
+
+void TimeSet(void) //时间设置功能
+{
+	if (KeyNum == 2) //按键2按下
+	{
+		TimeSetSelect++;	//设置选择位加1
+		TimeSetSelect %= 6; //越界清零
+	}
+	if (KeyNum == 3) //按键3按下
+	{
+		DS1302_Time[TimeSetSelect]++; //时间设置位数值加1
+		if (DS1302_Time[0] > 99)
+		{
+			DS1302_Time[0] = 0;
+		} //年越界判断
+		if (DS1302_Time[1] > 12)
+		{
+			DS1302_Time[1] = 1;
+		} //月越界判断
+		if (DS1302_Time[1] == 1 || DS1302_Time[1] == 3 || DS1302_Time[1] == 5 || DS1302_Time[1] == 7 ||
+			DS1302_Time[1] == 8 || DS1302_Time[1] == 10 || DS1302_Time[1] == 12) //日越界判断
+		{
+			if (DS1302_Time[2] > 31)
+			{
+				DS1302_Time[2] = 1;
+			} //大月
+		}
+		else if (DS1302_Time[1] == 4 || DS1302_Time[1] == 6 || DS1302_Time[1] == 9 || DS1302_Time[1] == 11)
+		{
+			if (DS1302_Time[2] > 30)
+			{
+				DS1302_Time[2] = 1;
+			} //小月
+		}
+		else if (DS1302_Time[1] == 2)
+		{
+			if (DS1302_Time[0] % 4 == 0)
+			{
+				if (DS1302_Time[2] > 29)
+				{
+					DS1302_Time[2] = 1;
+				} //闰年2月
+			}
+			else
+			{
+				if (DS1302_Time[2] > 28)
+				{
+					DS1302_Time[2] = 1;
+				} //平年2月
+			}
+		}
+		if (DS1302_Time[3] > 23)
+		{
+			DS1302_Time[3] = 0;
+		} //时越界判断
+		if (DS1302_Time[4] > 59)
+		{
+			DS1302_Time[4] = 0;
+		} //分越界判断
+		if (DS1302_Time[5] > 59)
+		{
+			DS1302_Time[5] = 0;
+		} //秒越界判断
+	}
+	if (KeyNum == 4) //按键3按下
+	{
+		DS1302_Time[TimeSetSelect]--; //时间设置位数值减1
+		if (DS1302_Time[0] < 0)
+		{
+			DS1302_Time[0] = 99;
+		} //年越界判断
+		if (DS1302_Time[1] < 1)
+		{
+			DS1302_Time[1] = 12;
+		} //月越界判断
+		if (DS1302_Time[1] == 1 || DS1302_Time[1] == 3 || DS1302_Time[1] == 5 || DS1302_Time[1] == 7 ||
+			DS1302_Time[1] == 8 || DS1302_Time[1] == 10 || DS1302_Time[1] == 12) //日越界判断
+		{
+			if (DS1302_Time[2] < 1)
+			{
+				DS1302_Time[2] = 31;
+			} //大月
+			if (DS1302_Time[2] > 31)
+			{
+				DS1302_Time[2] = 1;
+			}
+		}
+		else if (DS1302_Time[1] == 4 || DS1302_Time[1] == 6 || DS1302_Time[1] == 9 || DS1302_Time[1] == 11)
+		{
+			if (DS1302_Time[2] < 1)
+			{
+				DS1302_Time[2] = 30;
+			} //小月
+			if (DS1302_Time[2] > 30)
+			{
+				DS1302_Time[2] = 1;
+			}
+		}
+		else if (DS1302_Time[1] == 2)
+		{
+			if (DS1302_Time[0] % 4 == 0)
+			{
+				if (DS1302_Time[2] < 1)
+				{
+					DS1302_Time[2] = 29;
+				} //闰年2月
+				if (DS1302_Time[2] > 29)
+				{
+					DS1302_Time[2] = 1;
+				}
+			}
+			else
+			{
+				if (DS1302_Time[2] < 1)
+				{
+					DS1302_Time[2] = 28;
+				} //平年2月
+				if (DS1302_Time[2] > 28)
+				{
+					DS1302_Time[2] = 1;
+				}
+			}
+		}
+		if (DS1302_Time[3] < 0)
+		{
+			DS1302_Time[3] = 23;
+		} //时越界判断
+		if (DS1302_Time[4] < 0)
+		{
+			DS1302_Time[4] = 59;
+		} //分越界判断
+		if (DS1302_Time[5] < 0)
+		{
+			DS1302_Time[5] = 59;
+		} //秒越界判断
+	}
+	//更新显示，根据TimeSetSelect和TimeSetFlashFlag判断可完成闪烁功能
+	if (TimeSetSelect == 0 && TimeSetFlashFlag == 1)
+	{
+		LCD_ShowString(1, 1, "  ");
+	}
+	else
+	{
+		LCD_ShowNum(1, 1, DS1302_Time[0], 2);
+	}
+	if (TimeSetSelect == 1 && TimeSetFlashFlag == 1)
+	{
+		LCD_ShowString(1, 4, "  ");
+	}
+	else
+	{
+		LCD_ShowNum(1, 4, DS1302_Time[1], 2);
+	}
+	if (TimeSetSelect == 2 && TimeSetFlashFlag == 1)
+	{
+		LCD_ShowString(1, 7, "  ");
+	}
+	else
+	{
+		LCD_ShowNum(1, 7, DS1302_Time[2], 2);
+	}
+	if (TimeSetSelect == 3 && TimeSetFlashFlag == 1)
+	{
+		LCD_ShowString(2, 1, "  ");
+	}
+	else
+	{
+		LCD_ShowNum(2, 1, DS1302_Time[3], 2);
+	}
+	if (TimeSetSelect == 4 && TimeSetFlashFlag == 1)
+	{
+		LCD_ShowString(2, 4, "  ");
+	}
+	else
+	{
+		LCD_ShowNum(2, 4, DS1302_Time[4], 2);
+	}
+	if (TimeSetSelect == 5 && TimeSetFlashFlag == 1)
+	{
+		LCD_ShowString(2, 7, "  ");
+	}
+	else
+	{
+		LCD_ShowNum(2, 7, DS1302_Time[5], 2);
+	}
+}
+
 void main()
 {
 	LCD_Init();
@@ -49,123 +239,16 @@ void main()
 		}
 	}
 }
-/**
- * @brief  展示函数
- * @retval 无
- */
-// void showtime()
-// {
-// 	LCD_ShowNum(1, 1, Year, 4);
-// 	LCD_ShowNum(1, 6, Mon, 2);
-// 	LCD_ShowNum(1, 9, Day, 2);
-// 	LCD_ShowNum(2, 1, Hour, 2);
-// 	LCD_ShowNum(2, 4, Min, 2);
-// 	LCD_ShowNum(2, 7, Sec, 2);
-// }
 
-/**
- * @brief  掉电读取
- * @retval 无
- */
-// void read()
-// {
-// 	Year = AT24C02_ReadByte(110);
-// 	Mon = AT24C02_ReadByte(111);
-// 	Day = AT24C02_ReadByte(112);
-// 	Hour = AT24C02_ReadByte(113);
-// 	Min = AT24C02_ReadByte(114);
-// 	Sec = AT24C02_ReadByte(115);
-// }
-
-void Timer0_Rountine() interrupt 1 //中断函数,一般放在main.c里
+void Timer0_Routine() interrupt 1
 {
 	static unsigned int T0Count;
-	unsigned int t;	   //在中断函数内是局部变量，中断函数外是全局变量，static就是说下次再运行的时候，他的值不变，如果没有static，那么函数执行后变量就改变了
-	TH0 = 64535 / 256; //赋初值
-	TL0 = 64535 % 256;
+	TL0 = 0x18; //设置定时初值
+	TH0 = 0xFC; //设置定时初值
 	T0Count++;
-	//定时器过一秒 sec++
-	if (T0Count >= 1000)
+	if (T0Count >= 500) //每500ms进入一次
 	{
 		T0Count = 0;
+		TimeSetFlashFlag = !TimeSetFlashFlag; //闪烁标志位取反
 	}
-	// //大于60 min++
-	// if (Sec >= 60)
-	// {
-	// 	Min++;
-	// 	Sec = 0;
-	// }
-	// //大于60 hour++
-	// if (Min >= 60)
-	// {
-	// 	Hour++;
-	// 	Min = 0;
-	// }
-	// //大于24 day++
-	// if (Hour >= 24)
-	// {
-	// 	Hour = 0;
-	// 	Day++;
-	// }
-	// //重置闰年判断条件
-	// t = 0;
-	// //闰年判断
-	// if ((Year % 4 == 0 && Year % 100 != 0) || Year % 400 == 0)
-	// {
-	// 	t = 1;
-	// }
-	// //判断闰年月份情况
-	// if (t == 1)
-	// {
-	// 	if (Day > 29 && Mon == 2)
-	// 	{
-	// 		Mon++;
-	// 		Day = 1;
-	// 	}
-	// 	else if (Day > 30 && (Mon == 4 || Mon == 6 || Mon == 9 || Mon == 11))
-	// 	{
-	// 		Mon++;
-	// 		Day = 1;
-	// 	}
-	// 	else if (Day > 31 && (Mon == 1 || Mon == 3 || Mon == 5 || Mon == 7 || Mon == 8 || Mon == 10 || Mon == 12))
-	// 	{
-	// 		Mon++;
-	// 		Day = 1;
-	// 	}
-	// }
-	// //判断平年月份情况
-	// if (t != 1)
-	// {
-	// 	if (Day > 28 && Mon == 2)
-	// 	{
-	// 		Mon++;
-	// 		Day = 1;
-	// 	}
-	// 	else if (Day > 31 && (Mon == 1 || Mon == 3 || Mon == 5 || Mon == 7 || Mon == 8 || Mon == 10 || Mon == 12))
-	// 	{
-	// 		Mon++;
-	// 		Day = 1;
-	// 	}
-	// 	else if (Day > 30 && (Mon == 4 || Mon == 6 || Mon == 9 || Mon == 11))
-	// 	{
-	// 		Mon++;
-	// 		Day = 1;
-	// 	}
-	// }
-	// //判断月份是否进入下一年
-	// if (Mon > 12)
-	// {
-	// 	Year++;
-	// 	Mon = 1;
-	// }
-	// //重开世纪超过4位数
-	// if (Year > 9999)
-	// {
-	// 	Year = 0;
-	// 	Mon = 0;
-	// 	Day = 0;
-	// 	Hour = 0;
-	// 	Min = 0;
-	// 	Sec = 0;
-	// }
 }
